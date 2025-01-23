34ed308f76f1c48c0e95a887b37527157105bf3c
TheJadePhoenix <mattgmez98@gmail.com>
2025-01-23 00:14:57 -0500

    Created base Projectile class
    Projectile class holds hitbox and velocity2d reference and function for setting damage, max speed, and acceleration
    Projectile class is scripted by default to move in a forward (local +X) direction automatically
    Projectile scene has Velocity2D, Hitbox, and placeholder Sprite2D and is meant to be inherited from in future projectile scenes

---
+++ scenes/gameplay/projectiles/projectile.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+class_name Projectile
+
+
+@onready var hitbox := $Hitbox as Hitbox
+@onready var velocity_2d := $Velocity2D as Velocity2D
+
+
+func _process(delta: float) -> void:
+	var direction := (to_global(Vector2.RIGHT) - global_position).normalized()
+	velocity_2d.accelerate_in_direction(direction)
+	velocity_2d.move(self)
+	print(direction)
+
+
+func set_values(damage: float, speed: float, acceleration := velocity_2d.acceleration) -> void:
+	hitbox.set_damage(damage)
+	velocity_2d.max_speed = speed
+	velocity_2d.acceleration = acceleration
